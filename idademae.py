be865f4507c0c0bb6ca8368906622291f04ae4f3
Victoria Matos <victoria@quickup.io>
2024-10-03 16:30:03 -0300

    Implementa código que realiza cálculo para descobrir a idade da sua mãe

---
+++ idademae.py
@@ -0,0 +1,5 @@
+idade_mae_nasceu = int(input("Quer saber a idade da sua mae? Digite a idade dela quando ela te teve:"))
+sua_idade = int(input("Digite a sua idade:"))
+
+idade_mae = idade_mae_nasceu + sua_idade
+print("Idade da sua mae: ", idade_mae)
